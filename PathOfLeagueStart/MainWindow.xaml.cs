3e6c7d3586ffc6fb2df9023047fb7d4b4136f0d6
Johnathan DeLeeuw <john.m.deleeuw@gmail.com>
2020-06-16 00:35:52 -0500

    Initial Commit. XAML Structure, beginning of code. Initial files to be used to store previous config and initial menu data.

--- PathOfLeagueStart/MainWindow.xaml.cs
+++ PathOfLeagueStart/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace PathOfLeagueStart
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void populateListBox()
+        {
+            string line;
+            int columnToWriteTo;
+            System.IO.StreamReader file = new StreamReader("listBoxData.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("COL"))
+                {
+                    //columnToWriteTo = 
+                }
+            }
+
+        }
+
+        private void populateListBox(string selectedItem)
+        {
+            // will populate skill gem or support gem list box depending on name of item selected
+            // ie: if you pass through magic it will ignore weapon choice and fill in all spells in skill list box
+            // ie: if you pass through 2h axe and have selected melee it will fill the skill gems with all gems able to be used by 2h Axes in the skill gem list box
+            // ie: if you pass through Sunder it will fill in all supports able to support Sunder in the support list box using the selected weapon from weapon list box
+        }
+
+        private void ListBoxType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.UpdateGUI();
+            this.UpdateConfig();
+        }
+
+        private void UpdateGUI()
+        {
+            // Update GUI
+        }
+
+        private void UpdateConfig()
+        {
+
         }
     }
 }
